234a11674e5e947ea95dae80c6fa09aaf684393a
team135 <robotics@phm.k12.in.us>
2024-02-22 17:47:40 -0500

    Update SwerveC.java

--- src/main/java/frc/robot/commands/SwerveC.java
+++ src/main/java/frc/robot/commands/SwerveC.java
@@ -46,7 +46,7 @@ public class SwerveC extends Command {
     ySpeed = Math.pow(ySpeed, 2) * (ySpeed < 0 ? -1 : 1);
     turningSpeed = Math.pow(turningSpeed, 2) * (turningSpeed < 0 ? -1 : 1);
 
-    if (swerveS.autoLock == true) {
+    if (SwerveS.autoLock == true) {
       turningSpeed = autoLockController.calculate(swerveS.getXError(), 0.0)*-1;
       SmartDashboard.putNumber("Spin", turningSpeed);
     }
@@ -54,7 +54,7 @@ public class SwerveC extends Command {
     // If the desired ChassisSpeeds are really small (ie from controller drift) make them even smaller so that the robot doesn't move
     xSpeed = Math.abs(xSpeed) > Constants.SwerveConstants.kDeadband ? xSpeed : 0.0001;
     ySpeed = Math.abs(ySpeed) > Constants.SwerveConstants.kDeadband ? ySpeed : 0.0001;
-    if (swerveS.autoLock == true) {
+    if (SwerveS.autoLock == true) {
       turningSpeed = Math.abs(turningSpeed) > Constants.SwerveConstants.kAutoDeadband ? turningSpeed : 0.0001;
     } else {
       turningSpeed = Math.abs(turningSpeed) > Constants.SwerveConstants.kDeadband ? turningSpeed : 0.0001;
