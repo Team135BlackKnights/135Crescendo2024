95060813523e0cbf8b2ce9ac335c714bd933d3ca
PaidShill <112586790+PaidShill@users.noreply.github.com>
2024-03-11 12:45:05 -0400

    Friendship ended with color sensor, beam break sensor is my friend now

--- src/main/java/frc/robot/commands/autoCommands/AutonIntake.java
+++ src/main/java/frc/robot/commands/autoCommands/AutonIntake.java
@@ -24,8 +24,6 @@ public class AutonIntake extends Command {
 
     @Override
     public void execute() {
-        IntakeS.detected = IntakeS.colorSensorV3.getColor();
-        IntakeS.colorMatchResult = IntakeS.colorMatch.matchClosestColor(IntakeS.detected);
         if (timer.get() > 0.125) {
             isFinished = true;
         }
