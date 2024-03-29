850b5fd84c4c843ad80f740314fecc20b90b4adc
PaidShill <112586790+PaidShill@users.noreply.github.com>
2024-03-29 15:08:42 -0400

    Indentation Fixes
    Still need to fix the "create new files if there is a file called latest" idea

--- src/main/java/frc/robot/DataHandler.java
+++ src/main/java/frc/robot/DataHandler.java
@@ -8,7 +8,7 @@ public class DataHandler{
     public static FileOutputStream outputStream;
     public static OutputStreamWriter outputStreamWriter;
     String[] loggingArray;
-    public static int id = 999999;
+    public static int id = 0;
     /**
      * Creates a file on a USB attached to the rio.
      * Used in conjunction with https://colab.research.google.com/drive/1pj7j6u2x-s2kY2aWgmiyc5_2wwpUgusc. ONLY CALL ONCE OR IT'LL BRICK!
@@ -20,56 +20,62 @@ public class DataHandler{
     //function based off of https://github.com/HoyaRobotics/InfiniteRecharge2020/blob/master/src/main/java/frc/robot/util/Logger.java
     public static void setUpLogOnUSB(){
 
-    try {
-        //Creates new file 
-        String fileName = "C://logs/Latest.txt";
-        File createdFile = new File(fileName);
-        //if it exists, rename file with same name to the id in its first line
-        if (createdFile.exists()){
-            //Renames a file if the logfile exists
-            Scanner renameScanner = new Scanner(createdFile);
-            id = Integer.parseInt(renameScanner.nextLine());
-            File newFileName = new File("C://logs" + id + ".txt");
-            createdFile.renameTo(newFileName);
-            renameScanner.close();
-            
-          } 
-        createdFile.createNewFile();
+        try {
         
-        outputStream = new FileOutputStream(createdFile);
-        outputStreamWriter = new OutputStreamWriter(outputStream);
-        id +=1; 
-        outputStreamWriter.write(id + System.lineSeparator());
-        outputStreamWriter.flush();
+            //Creates new file 
+            String fileName = "C://logs/Latest.txt";
+            File createdFile = new File(fileName);
+            
+            //if a file named "latest" exists, rename "latest" to the id in its first line
+            if (createdFile.exists()){
+                //TODO: Get this renaming to work
+                    
+                    Scanner renameScanner = new Scanner(createdFile);
+                    id = Integer.parseInt(renameScanner.nextLine());
+                    File newFileName = new File("C://logs/Log" + id + ".txt");
+                    System.out.println(newFileName);
+                    createdFile.renameTo(newFileName);
+                    renameScanner.close();
+            } 
+
+            createdFile.createNewFile();
+    
+            id +=1; 
+            outputStream = new FileOutputStream(createdFile);
+            outputStreamWriter = new OutputStreamWriter(outputStream);
+            outputStreamWriter.write(id + "\r\n");
+            outputStreamWriter.flush();
         } 
-            //catch any errors
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            catch (IOError e){
-                e.printStackTrace();
-            }
+        //catch any errors
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        catch (IOError e){
+            e.printStackTrace();
+        }
     }
     /**
      * Creates an array of strings, each string represents a table entry to be logged. 
      * Recommended to start by logging the table heading names first though
      */
     public static void logData(String[] tableHeadings){
+        
         String lineToBeSaved= "";
-        //
+
         for (String heading : tableHeadings){
             lineToBeSaved += (heading + ",");
         }
         lineToBeSaved = lineToBeSaved.substring(0, (lineToBeSaved.length()-1));
         try {
-            outputStreamWriter.write(lineToBeSaved + System.lineSeparator());
+            outputStreamWriter.write(lineToBeSaved + "\r\n");
             outputStreamWriter.flush();
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
             e.printStackTrace();
         }
         catch (IOError e){
-                e.printStackTrace();
-            }
+            e.printStackTrace();
+        }
     }
     /**
      * Closes writer at the end, might not be used
@@ -77,11 +83,12 @@ public class DataHandler{
     public static void closeWriter(){
         try {
             outputStreamWriter.close();
-        } catch (Exception e) {
-            // TODO: handle exception
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
         }
         catch (IOError e){
-                e.printStackTrace();
-            }
+            e.printStackTrace();
+        }
     }
 }
