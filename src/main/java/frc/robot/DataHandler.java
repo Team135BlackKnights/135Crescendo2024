e1da8920bdb96fdd01d4abfa8b65d4e5ce431105
PaidShill <112586790+PaidShill@users.noreply.github.com>
2024-03-29 15:48:38 -0400

    Commented Code
    for if we ever use this in the future

--- src/main/java/frc/robot/DataHandler.java
+++ src/main/java/frc/robot/DataHandler.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.IOError;
-public class DataHandler{
+
+/**
+ * Class used for logging data. Coded as an alternative to DataLogManager (so you don't get every NetworkTables update logged), designed to be used for polynomial regression. 
+ * Used in conjunction with https://colab.research.google.com/drive/1pj7j6u2x-s2kY2aWgmiyc5_2wwpUgusc to output an accurate polynomial model linking 2 variables together.
+ * Can also be used as a way to save values on a .txt file to the RIO. To use this with the polynomial regression tool, change the endings of all the files to .csv, and run it.
+ * Saves data in columns instead of rows (each value for x is represented by the values below it). 
+ */
+
+ public class DataHandler{
+
     public static FileOutputStream outputStream;
     public static OutputStreamWriter outputStreamWriter;
     String[] loggingArray;
@@ -12,10 +21,7 @@ public class DataHandler{
     public static String directoryName = "E://logs/";
     public static File newFileName;
     /**
-     * Creates a file on a USB attached to the rio.
-     * Used in conjunction with https://colab.research.google.com/drive/1pj7j6u2x-s2kY2aWgmiyc5_2wwpUgusc. ONLY CALL ONCE OR IT'LL BRICK!
-     * @return
-     * FileName: The file name of the created file, or an error if it could not be created
+     * Creates a file on a USB attached to the rio to log all values to. ONLY CALL ONE TIME EVER (either in RobotInit or a subsystem constructor)
      */
 
     // "/U" is the default directory for RoboRIO flash drives, add new file to "/U/logs/"
@@ -30,7 +36,6 @@ public class DataHandler{
             
             //if a file named "latest" exists, rename "latest" to the id in its first line
             if (createdFile.exists()){
-                //TODO: Get this renaming to work
 
                 Scanner renameScanner = new Scanner(createdFile);
                 if (renameScanner.hasNext()){
@@ -38,7 +43,8 @@ public class DataHandler{
                     renameScanner.close();
                     System.out.println(newFileName);
                 }
-                
+
+                //Create the new file
                 File newFileName = new File(directoryName + "Log" + id + ".txt");
                 createdFile.renameTo(newFileName);
 
@@ -61,8 +67,10 @@ public class DataHandler{
         }
     }
     /**
-     * Creates an array of strings, each string represents a table entry to be logged. 
-     * Recommended to start by logging the table heading names first though
+     * Writes values to file
+     * Recommended to start by logging the table heading names first, polynomial regression tool handles this. If there is a time when you want to log data but want to ignore something, put null in as the value in the array 
+     * (will not be handled by regression calculator as of now)
+     * @param tableHeadings the array of values to be logged, can be different from the values declared in the setUpLogOnUsb
      */
     public static void logData(String[] tableHeadings){
         
@@ -84,7 +92,7 @@ public class DataHandler{
         }
     }
     /**
-     * Closes writer at the end, might not be used
+     * Closes writer at the end, does not need to be used
      */
     public static void closeWriter(){
         try {
