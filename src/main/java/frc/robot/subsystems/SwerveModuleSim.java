a711cd422260bb489194bba15e49eb412c05fa04
PaidShill <112586790+PaidShill@users.noreply.github.com>
2024-04-29 23:26:33 -0400

    Added some more code
    Needs actual updating functions

--- src/main/java/frc/robot/subsystems/SwerveModuleSim.java
+++ src/main/java/frc/robot/subsystems/SwerveModuleSim.java
@@ -1,15 +1,26 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import frc.robot.Constants.DriveSimConstants;
 public class SwerveModuleSim {
-    public SwerveModuleSim(double driveKa, double steerKa, double driveKv, double steerKv){
+    private double driveMaxVoltage = 12;
+    private double steerMaxVoltage = 12;
+    LinearSystem<N1, N1, N1> steerMotorLinearSystem;
+    LinearSystem<N2, N1, N1> driveMotorLinearSystem;
+    public SwerveModuleSim(CANSparkMax drive, CANSparkMax steer, double[] steerKvKaKs, double[] driveKvKaKs){
         //We should need one position linear system and one velocity linear system
-        final LinearSystem<N1, N1, N1> steerMotorLinearSystem = LinearSystemId.identifyVelocitySystem(steerKv, steerKa);
-        final LinearSystem<N2, N1, N1> driveMotorLinearSystem = LinearSystemId.identifyPositionSystem(driveKv, driveKa); 
+        steerMotorLinearSystem = LinearSystemId.identifyVelocitySystem(steerKvKaKs[0], steerKvKaKs[1]);
+        driveMotorLinearSystem = LinearSystemId.identifyPositionSystem(driveKvKaKs[0], driveKvKaKs[1]); 
+        
+    }
+    public void setDrive(double percentVoltage){
+        double voltage = driveMaxVoltage*percentVoltage;
+        double sign = Math.signum(voltage);
         
     }
 }
